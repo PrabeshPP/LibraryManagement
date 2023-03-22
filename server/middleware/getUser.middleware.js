c24fedc955c1cd2f3c83cc38e6d54de54e37ecc7
PrabeshPP <prabesh.bista369@gmail.com>
2023-03-22 15:23:36 -0400

    [added] getUser function

---
+++ server/middleware/getUser.middleware.js
@@ -0,0 +1,8 @@
+const { verifyToken } = require("../utils/token");
+
+const getCurrentUser=(req,res,next)=>{
+    const header=req.headers.authorization;
+    const token=header.split(" ")[1];
+    const result=verifyToken({"token":token});
+    
+}
