5469a0093a05fd79a721f7e82de16b8f981c9eb0
PrabeshPP <prabesh.bista369@gmail.com>
2023-03-24 00:32:27 -0400

    [working] frontend

--- server/views/src/components/page/normalPage.js
+++ server/views/src/components/page/normalPage.js
@@ -1,6 +1,40 @@
-const NormalPage=()=>{
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
+const NormalPage = () => {
+    const [books, setBooks] = useState([]);
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("/books");
+            if (response) {
+                const arrData = await response.data.books
+                arrData.map((book) => {
+                    setBooks((prevData) => {
+                        return [...prevData, book]
+                    })
+                })
+            } else {
+                setBooks([]);
+            }
+        } catch (err) {
+
+        }
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
     return <div className=" h-[90vh] w-[100%] text-3xl font-bold flex justify-center items-center">
-        Welcome! to the Normal Page.
+        {books.length!=0?<div className="h-[100%] w-[100%]">
+            {
+                books.map((book)=>{
+                    return <div className="h-[20vh] w-[30%] bg-red-500">
+                        <p>{book.bookName}</p>
+                    </div>
+                })
+            }
+        </div>:<div className=" animate-spin h-[40px] w-[40px] rounded-full border-t-2 border-t-bla
+         border-r-2 border-r-black border-b-2 border-b-black"></div>}
     </div>
 }
 
