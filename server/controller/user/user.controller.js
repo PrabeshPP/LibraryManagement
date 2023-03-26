bce70f819f575956bd79c78609f59b12c8fd862a
PrabeshPP <prabesh.bista369@gmail.com>
2023-03-26 01:07:58 -0400

    [added] delete and update code in the server side

--- server/controller/user/user.controller.js
+++ server/controller/user/user.controller.js
@@ -1,4 +1,24 @@
+const Prisma=require("../../utils/prisma");
 //delete the user
+const deleteUser=async(req,res)=>{
+    const userId=req.body.id;
+    const deletedRefreshToken=await Prisma.refreshToken.deleteMany({
+        where:{
+            userId:userId
+        }
+    })
+    const deletedUser=await Prisma.user.delete({
+        where:{
+            id:userId
+        }
+    })
+    res.status(200)
+    res.json({"message":"Successfully Deleted the User"})
+}
 
 
 //update the user
+
+const updateUser=async(req,res)=>{
+    
+}
