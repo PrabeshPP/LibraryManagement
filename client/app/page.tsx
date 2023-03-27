0293d0a8c531a1e462cb35ed627d2159190b80da
Sanij Shrestha <saniz.sth123@gmail.com>
2023-03-27 00:31:00 -0400

    add reserve button

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -25,20 +25,21 @@ export default async function Home() {
   console.log(data)
   return (
     <main className="w-[100%] min-h-[90vh]">
-      <div className=" min-h-[90vh] w-[100%] flex flex-wrap justify-around">
+      <div className=" min-h-[90vh] w-[100%] flex flex-wrap justify-around mt-6">
        {
         data.length===0?<div>Loading......</div>:data.map((book)=> {
-          return <div className="h-1/4 w-1/4 font-sans rounded-lg bg-[#f8f8f9] m-2">
+          return <div className="h-1/4 w-1/4 font-sans rounded-lg bg-[#f8f8f9] m-2 ">
             <Image
             src = {book.coverImage}
-            className=""
-            width={300}
+            className="mx-auto my-auto"
+            width={200}
+            height= {144}
             alt ="ksdjnfs"
-            height= {100}
             />
-            <h4 className="text-xl">{book.bookName}</h4>
-            <span className="text-sm">{book.isbn}</span>
-            <p className="text-base">{book.summary}</p>
+            <h4 className="text-xl ml-2 mt-2 text-[#3b78ce]">{book.bookName}</h4>
+            <span className="text-xs ml-2">ISBN: {book.isbn}</span>
+            <p className="text-base ml-2">{book.summary}</p>
+            <button className="rounded-lg bg-[#3b78ce] items-center text-white p-2 m-2 font-mono">Reserve</button>
           </div>
         })
        }
